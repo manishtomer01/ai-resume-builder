bb0a5708b1042efb73d589c76e7396465aed9966
Manish Tomer <mt0578902@gmail.com>
2025-01-01 19:03:58 +0530

    exp

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,11 +1,13 @@
 import { ReactNode } from "react";
 import Navbar from "./Navbar";
+import PremiumModal from "@/components/premium/PremiumModal";
 
 export default function layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col">
       <Navbar />
       {children}
+      <PremiumModal />
     </div>
   );
 }
--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -1,11 +1,8 @@
-import React from "react";
-import { Metadata } from "next";
-import { PlusSquare } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import prisma from "@/lib/prisma";
 import { resumeDataInclude } from "@/lib/types";
 import { auth } from "@clerk/nextjs/server";
+import { Metadata } from "next";
+import CreateResumeButton from "./CreateResumeButton";
 import ResumeItem from "./ResumeItem";
 
 export const metadata: Metadata = {
@@ -35,12 +32,7 @@ export default async function page() {
 
   return (
     <main className="mx-auto w-full max-w-7xl space-y-6 px-3 py-6">
-      <Button asChild className="mx-auto flex w-fit gap-2">
-        <Link href="/editor">
-          <PlusSquare className="size-5" />
-          New Resume
-        </Link>
-      </Button>
+      <CreateResumeButton canCreate={totalCount < 3} />
       <div className="space-y-1">
         <h1 className="text-3xl font-bold">Your resumes</h1>
         <p>Total: {totalCount}</p>
